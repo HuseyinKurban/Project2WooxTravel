3ba8c016f2fed8fd491318b79165006961498e44
Hüseyin Kurban <huseyinkurban03@gmail.com>
2024-10-08 20:46:07 +0300

    Add project files.

---
+++ Project2WooxTravel/Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project2WooxTravel.Entities
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        [StringLength(100)]
+        public string CategoryName { get; set; }
+        public string CategoryStatus { get; set; }
+    }
+}
