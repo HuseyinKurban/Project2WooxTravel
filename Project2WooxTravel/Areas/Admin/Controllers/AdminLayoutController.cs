513843db248b28ec06aa715d3cb68430fafdb596
Hüseyin Kurban <huseyinkurban03@gmail.com>
2024-10-13 23:45:20 +0300

    Add project files.

--- Project2WooxTravel/Areas/Admin/Controllers/AdminLayoutController.cs
+++ Project2WooxTravel/Areas/Admin/Controllers/AdminLayoutController.cs
@@ -13,5 +13,10 @@ namespace Project2WooxTravel.Areas.Admin.Controllers
         {
             return View();
         }
+
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
     }
 }
